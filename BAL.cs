debf3ac04b7267d0b5aac7e2154a3f9e93b9c40b
nisha1230 <49358797+nisha1230@users.noreply.github.com>
2021-06-10 12:30:20 +0530

    Add files via upload

---
+++ BAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessAcessLayer
+{
+    public class BAL
+    {
+        
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        DAL da = new DAL();
+        public bool getUser()
+        {
+            if (da.IsValid(UserName,Password).Tables[0].Rows.Count >  0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
