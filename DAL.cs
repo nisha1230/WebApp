15e754af03dcfb330973006bd93039ed2b58b6b5
nisha1230 <49358797+nisha1230@users.noreply.github.com>
2021-06-10 12:29:21 +0530

    Add files via upload

---
+++ DAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DAL
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DBcon"].ConnectionString;
+
+        public DataSet IsValid(string UserName, string Password)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_CheckLoginDetails ";
+            cmd.Parameters.AddWithValue("@UserName", UserName);
+            cmd.Parameters.AddWithValue("@Password", Password );
+            SqlDataAdapter sda = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            cmd.Connection = connection;
+            sda.SelectCommand = cmd;
+            sda.Fill(ds);
+            return ds;
+            
+        }
+    }
+}
